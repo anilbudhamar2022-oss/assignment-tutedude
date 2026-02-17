67b0459fccb1275339739e2d4d3c6d15e97e322e
anilandra019 <anilbudhamar019@gmail.com>
2026-02-17 12:34:56 +0530

    Adding task files

---
+++ assigment1/task1.py
@@ -0,0 +1,12 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+add = int(num1) + int(num2)
+sub = int(num1) - int(num2)
+mul = int(num1) * int(num2)
+div = int(num1) / int(num2)
+
+print("Addition: ", add)
+print("Substraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
